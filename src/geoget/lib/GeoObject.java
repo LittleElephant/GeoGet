ab283b5d3efc2ed471a30cdb18ef2bd20d3edd9f
Plutalov <tingel@list.ru>
2014-04-24 01:56:19 +0400

    Added meter converter

--- src/geoget/lib/GeoObject.java
+++ src/geoget/lib/GeoObject.java
@@ -5,13 +5,21 @@ public class GeoObject {
     public double lat;
     public double lon;
 
-    public GeoObject (String name, double lat, double lon) {
+    public GeoObject (String name, double lon, double lat) {
         this.name = name;
-        this.lat = lat;
         this.lon = lon;
+        this.lat = lat;
     }
 
     public String toString() {
         return String.format("GeoObject(%s %f %f)", name, lat, lon);
+    }
+
+    public double distance(GeoObject other) {
+        return Math.sqrt(Math.pow(this.lat - other.lat, 2) + Math.pow(this.lon - other.lon, 2));
+    }
+
+    public static double convertToMeters(double grads) {
+        return grads * 111 * 1000;
     }
 }
