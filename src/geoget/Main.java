8c31a492189ed7456f6f59723f04bd7f3671d920
Plutalov <tingel@list.ru>
2014-04-26 18:27:50 +0400

    Getting Json moved to YandexGeoApiRequest.

--- src/geoget/Main.java
+++ src/geoget/Main.java
@@ -2,15 +2,8 @@ package geoget;
 
 import geoget.lib.GeoObject;
 import geoget.lib.YandexGeoApiRequest;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 
 public class Main {
 
@@ -18,56 +11,37 @@ public class Main {
 		YandexGeoApiRequest req;
 		double lon=37.619559, lat=55.739789;
 		req = new YandexGeoApiRequest(lon, lat);
-		JSONParser parser = new JSONParser();
-	    GeoObject center = new GeoObject("Center", lon, lat);
-		
-		try {
-			URL url = new URL(req.getRequest());
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			BufferedReader in = new BufferedReader(
-				new InputStreamReader(conn.getInputStream()));
-			String inputLine;
-			StringBuilder json = new StringBuilder();
-			while ((inputLine = in.readLine()) != null) {
-				json.append(inputLine);
-			}
-			JSONArray data;
-			JSONObject jsonParsed =	(JSONObject) parser.parse(json.toString());
+        JSONObject jsonParsed = req.getJson();
+        GeoObject center = new GeoObject("Center",lon,lat);
+
+
+        JSONArray data;
 			jsonParsed = (JSONObject) jsonParsed.get("response");
 			jsonParsed = (JSONObject) jsonParsed.get("GeoObjectCollection");
 			data = (JSONArray) jsonParsed.get("featureMember");
-			ArrayList<GeoObject> geoObjects = new ArrayList<GeoObject>();
 			for (Object obj: data){
-				jsonParsed = (JSONObject) obj;
-				jsonParsed = (JSONObject) jsonParsed.get("GeoObject");
-				JSONObject point = (JSONObject) jsonParsed.get("Point");
-				jsonParsed = (JSONObject) jsonParsed.get("metaDataProperty");
-				jsonParsed = (JSONObject) jsonParsed.get("GeocoderMetaData");
-				jsonParsed = (JSONObject) jsonParsed.get("AddressDetails");
-				jsonParsed = (JSONObject) jsonParsed.get("Country");
-                String[] position = ((String) point.get("pos")).split(" ");
+            jsonParsed = (JSONObject) obj;
+            jsonParsed = (JSONObject) jsonParsed.get("GeoObject");
+            JSONObject point = (JSONObject) jsonParsed.get("Point");
+            jsonParsed = (JSONObject) jsonParsed.get("metaDataProperty");
+            jsonParsed = (JSONObject) jsonParsed.get("GeocoderMetaData");
+            jsonParsed = (JSONObject) jsonParsed.get("AddressDetails");
+            jsonParsed = (JSONObject) jsonParsed.get("Country");
+            String[] position = ((String) point.get("pos")).split(" ");
 
-                GeoObject current = new GeoObject(
-                        (String)jsonParsed.get("AddressLine"),
-                        Double.parseDouble(position[0]),
-                        Double.parseDouble(position[1])
-                );
-                geoObjects.add(current);
-                System.out.println(
-                        String.format(
-                                "%s\t%.0fм",
-                                current.name,
-                                center.distance(current)
-                        )
-                );
-
-            }
+            GeoObject current = new GeoObject(
+                    (String)jsonParsed.get("AddressLine"),
+                    Double.parseDouble(position[0]),
+                    Double.parseDouble(position[1])
+            );
+            System.out.println(
+                    String.format(
+                            "%s\t%.0fм",
+                            current.name,
+                            center.distance(current)
+                    )
+            );
         }
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		
-	}
+    }
 }
 
--- src/geoget/lib/YandexGeoApiRequest.java
+++ src/geoget/lib/YandexGeoApiRequest.java
@@ -1,16 +1,40 @@
 package geoget.lib;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Locale;
 
 public class YandexGeoApiRequest {
-	private String baseformat = "http://geocode-maps.yandex.ru/1.x/?format=json&kind=house&spn=0.005,0.005&results=100&geocode=%.6f,%.6f";
-	private String request;
+    private String request;
 	
 	public YandexGeoApiRequest(double lon, double lat) {
-		request = String.format(Locale.US, baseformat, lon, lat);
-	}
-	
-	public String getRequest() {
-		return request;
-	}
+        String baseformat = "http://geocode-maps.yandex.ru/1.x/?format=json&kind=house&spn=0.005,0.005&results=100&geocode=%.6f,%.6f";
+        request = String.format(Locale.US, baseformat, lon, lat);
+    }
+
+    public JSONObject getJson() {
+        JSONParser parser = new JSONParser();
+
+        JSONObject jsonParsed = new JSONObject();
+        try {
+            URL url = new URL(request);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuilder json = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                json.append(inputLine);
+            }
+            jsonParsed = (JSONObject) parser.parse(json.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jsonParsed;
+    }
 }
