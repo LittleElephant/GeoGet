33262ad9eefaeafab4c42d2d9f98bb5c838fb3b5
cassum <r.degrik@yandex.ru>
2014-04-29 03:44:39 +0400

    no magic vars

--- src/geoget/Main.java
+++ src/geoget/Main.java
@@ -6,11 +6,11 @@ import geoget.lib.GeoObjectCollection;
 public class Main {
 
 	public static void main(String[] args) {
-        double lon = 37.619559, lat = 55.739789;
+        double lon = 37.619559, lat = 55.739789, spn = 0.007;
         GeoObjectCollection collection = new GeoObjectCollection();
         GeoObject center = new GeoObject("Center", lon, lat);
 
-        for (GeoObject current: collection.centers(lon, lat, 0.007)){
+        for (GeoObject current: collection.centers(lon, lat, spn)){
             System.out.println(String.format("%s\t%04.0fм", current.name, current.distance(center)));
         }
     }
