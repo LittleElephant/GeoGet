cef7bbba3f8c7639729c0ef2ebfd9df98cc13e87
Plutalov <tingel@list.ru>
2014-05-01 10:00:23 +0400

    Created GeoObjectCollection ArrayList

--- src/geoget/Main.java
+++ src/geoget/Main.java
@@ -6,7 +6,7 @@ import geoget.lib.GeoObjectCollection;
 public class Main {
 
 	public static void main(String[] args) {
-        double lon = 37.619559, lat = 55.739789, spn = 0.007;
+        double lon = 37.619559, lat = 55.739789, spn = 0.0059;
         GeoObjectCollection collection = new GeoObjectCollection();
         GeoObject center = new GeoObject("Center", lon, lat);
 
